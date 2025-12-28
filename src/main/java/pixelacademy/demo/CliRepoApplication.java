d7277f5d813f93ba5fca0224e8dffc8005da0880
Pusca Vasile <vasea-pusca@mail.ru>
2025-12-28 12:52:39 +0200

    Initial commit

---
+++ src/main/java/pixelacademy/demo/CliRepoApplication.java
@@ -0,0 +1,13 @@
+package pixelacademy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliRepoApplication.class, args);
+	}
+
+}
